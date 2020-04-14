5147935c3015db7c68ab3c340408ed6315b52e13
FrankieDLT <51407226+FrankieDLT@users.noreply.github.com>
2020-04-14 14:01:05 -0500

    Checkstyle
    Fixing checkstyle errors

--- src/main/java/com/iteso/greenwar/StartCurrentGame.java
+++ src/main/java/com/iteso/greenwar/StartCurrentGame.java
@@ -1,12 +1,42 @@
 package com.iteso.greenwar;
 
+/**
+ * This class will act as the selection
+ * class for the dificulty of the game.
+ * @author Juan Carlos
+ * @version 06/04/2020/1.0
+ * */
 public class StartCurrentGame {
+    /**
+     * Timer.
+     * @author Juan Carlos
+     * */
     private final int departureTime = 13;
+    /**
+     * Low tier dificulty quiz.
+     * @author Juan Carlos
+     * */
     private final int littleQuiz = 20;
+    /**
+     * Medium tier dificulty quiz.
+     * @author Juan Carlos
+     * */
     private final int middleQuiz = 35;
+    /**
+     * High tier dificulty quiz.
+     * @author Juan Carlos
+     * */
     private final int bigQuiz = 50;
 
-    public int chooseQuiz(int selectionNumber){
+    /**
+     * This class will act as the selection.
+     * method for the dificulty of the game
+     * @author Juan Carlos
+     * @version 06/04/2020/1.0
+     * @param selectionNumber Number user for the selection
+     * @return Dificulty to be used
+     * */
+    public int chooseQuiz(final int selectionNumber) {
         switch (selectionNumber) {
             case 0:
                 return littleQuiz;
--- src/main/java/com/iteso/greenwar/StartGame.java
+++ src/main/java/com/iteso/greenwar/StartGame.java
@@ -1,7 +1,20 @@
 package com.iteso.greenwar;
-
+/**
+ * This class will act as the initialization
+ * class for the game.
+ * @author Juan Carlos
+ * @version 06/04/2020/1.0
+ * */
 public class StartGame {
-    public static void main (String[] args){
+    /**
+     * This class is the main class for the game.
+     * @author Juan Carlos
+     * @version 06/04/2020/1.0
+     * @param args "array of strings which
+     *             stores arguments passed by command line
+     *             while starting a program"
+     * */
+    public  void main(final String[] args) {
 
     }
 }
